5bfb2b6bcef18d4a4f8bca57271b02b7f38f7dd5
Anita <anita.liva39@gmail.com>
2025-01-30 08:49:55 +0200

    updated a bit of things in account and added some dependencies  and deleted old console classes

--- src/BankAction.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-// idea is to make interface since we learned OOP and t not use switch cases but use interface and classes that override 
-public interface BankAction {
-    void execute(Scanner scanner, BankAccount account1, BankAccount account2);
-
-}
--- src/ExitAction.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ExitAction implements BankAction {
-    @Override
-    public void execute(Scanner scanner, BankAccount account1, BankAccount account2) {
-        System.out.println("Exiting.");
-        System.exit(0);
-    }
-}
